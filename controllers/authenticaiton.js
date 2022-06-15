ad59f7d71cee3cd25543bf1ed7e7ea020ed0ee48
Tobechukwu Ugwuanyi <john.ugwuanyi@yahoo.com>
2022-06-15 11:11:52 +0100

    'upd'

--- controllers/authenticaiton.js
+++ controllers/authenticaiton.js
@@ -12,25 +12,32 @@ exports.createUser = async (req, res, next) => {
 	const { userName, name, email, password } = req.body;
 
 	const existingUser = await User.findOne({ email: email });
+	const existingUsername = await User.findOne({ userName: userName });
 
 	const avatar = `https://avatars.dicebear.com/api/micah/${userName}.svg?background=%231B8968`;
 
+	if (existingUsername) {
+		return res
+			.status(400)
+			.json({ message: "Username is already taken", statusCode: 400 });
+	}
+
 	if (existingUser) {
 		return res
-			.status(401)
-			.json({ message: "User already exists", stausCode: 401 });
+			.status(400)
+			.json({ message: "User already exists", stausCode: 400 });
 	}
 
 	if (!validate.isEmail(email)) {
 		return res
-			.status(401)
-			.json({ message: "Enter a valide email address", statusCode: 401 });
+			.status(400)
+			.json({ message: "Enter a valid email address", statusCode: 400 });
 	}
 
 	if (validate.isEmpty(password) || !validate.isLength(password, { min: 5 })) {
 		return res
-			.status(401)
-			.json({ message: "Invalid Password", statusCode: 401 });
+			.status(400)
+			.json({ message: "Invalid Password", statusCode: 400 });
 	}
 
 	const hashedPassword = await bcrypt.hash(password, 12);
@@ -59,27 +66,25 @@ exports.login = async (req, res, next) => {
 	const user = await User.findOne({ email: email });
 
 	if (!user) {
-		return res
-			.status(401)
-			.json({ message: "Username or Password is incorrect" });
+		return res.status(401).json({ message: "User does not exist!" });
 	}
 
 	if (!validate.isEmail(email)) {
-		return res.status(401).json({ message: "Invalid Email", statusCode: 401 });
+		return res.status(400).json({ message: "Invalid Email", statusCode: 400 });
 	}
 
 	if (validate.isEmpty(password) || !validate.isLength(password, { min: 5 })) {
 		return res
-			.status(401)
-			.json({ message: "Password is too short", statusCode: 401 });
+			.status(400)
+			.json({ message: "Password is too short", statusCode: 400 });
 	}
 
 	const userPassword = await bcrypt.compare(password, user.password);
 
 	if (!userPassword) {
 		return res
-			.status(401)
-			.json({ message: "Username or Password is invalide", statusCode: 401 });
+			.status(400)
+			.json({ message: "Username or Password is invalid", statusCode: 400 });
 	}
 
 	const token = jwt.sign(
@@ -109,8 +114,8 @@ exports.forgotPassword = async (req, res, next) => {
 
 	if (!user) {
 		return res
-			.status(401)
-			.json({ message: "User does not exist!", statusCode: 401 });
+			.status(400)
+			.json({ message: "User does not exist!", statusCode: 400 });
 	}
 
 	const resetToken = jwt.sign({ _id: user._id }, "secret", {
@@ -120,14 +125,15 @@ exports.forgotPassword = async (req, res, next) => {
 	const updateResetLink = await user.updateOne({ resetLink: resetToken });
 
 	if (!updateResetLink) {
-		return res.status(401).json({
+		return res.status(400).json({
 			message: "Something went wrong, please try again!",
-			statusCode: 401,
+			statusCode: 400,
 		});
 	}
 
 	return res.status(200).json({
-		message: "Check your email for the reset link and follow the instructions",
+		message:
+			"Please check your email for the reset link and follow the instructions",
 		statusCode: 200,
 	});
 };
